3dc098a754de57fb387db25370415d1470fc6b0f
junhee <wjunhee102@gmail.com>
2020-04-21 22:23:02 +0900

    Vue start

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// new Vue({
+//     el: '#app',
+//     data() {
+//         return {
+//            userId: '',
+//            userPw: '' 
+//         }
+//     }
+// })
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+           items: [
+               {
+                   id: 1,
+                   image: 'https://picsum.photos/210/118/?image=1'
+               }
+           ]
+        }
+    }
+})
